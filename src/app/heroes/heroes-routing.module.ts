1b4ebb56e1074b5283df673573a9cd3c3fffaa4e
sebastian <qualitycolombia.dvp03@gmail.com>
2024-01-27 11:30:20 -0500

    first commit to app heroes

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+
+/** ROUTES HEROS with children routes */
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent, // main route
+    children: [
+      { path: 'new-hero', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: ':id', component: HeroPageComponent }, // ruta comodin, siempre poner de ultima
+      { path: '**', redirectTo: 'list' },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HeroesRoutingModule { }
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of, retry } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+import { environments } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class HeroesService {
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  // get all heroes
+  getHeroes(): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  // get hero by id and return if exist, and if not exist return undefine observable by catching the error
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError( error => of(undefined) ) // catch error - retorna observable que retorna undefine
+      );
+  }
+
+}
